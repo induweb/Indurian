3b6c719810fe80c9a7a7490f156a170564a5e8a1
Pawel Pyra <pawel.pyra@e-systemy.com>
2017-04-12 13:54:16 +0200

    info added - main text changed

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,12 @@ class AppComponent extends React.Component {
   render() {
     return (
       <div className="content">
-        <h1>Indurian Game!!!</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam fugiat, id laboriosam maiores reiciendis sequi tenetur vitae. Consequatur consequuntur corporis dolorum ducimus enim odit voluptatibus. Aspernatur deleniti distinctio natus odit totam. Aspernatur autem eius maiores, officia reiciendis sapiente voluptate. Aliquid delectus labore modi officiis sint sunt voluptates? Exercitationem illo maxime natus. Animi deserunt optio quia ut. Architecto at corporis deleniti harum hic impedit libero minima minus mollitia necessitatibus nesciunt nihil odit quam, quas sequi vero voluptatibus! Aliquid, eaque fugiat? Alias aliquam blanditiis commodi doloribus, est ex facere fuga fugiat, harum impedit, laboriosam laudantium modi nam nemo quas quo sit!</p>
-      </div>
+          <h1>Sterowanie</h1>
+          <p>Spacja - start gry</p>
+          <p>Strzałka w górę - poruszanie w górę</p>
+          <p>Strzałka w dół - poruszanie w dół</p>
+          <p>Strzałka w prawo - czarowanie</p>
+        </div>
     );
   }
 }
