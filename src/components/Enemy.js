a73d4c1e4556e3244244616db1794218a5586cd6
Pawel Pyra <pawel.pyra@e-systemy.com>
2017-04-22 13:42:44 +0200

    enemy status added

--- src/components/Enemy.js
+++ src/components/Enemy.js
@@ -5,11 +5,24 @@ class Crate extends React.Component {
 
   render () {
     return (
-        <div className={`enemy ${this.props.type} orc-${this.props.status}`} style={{left: this.props.left + 'px',
+        <div className={`enemy ${this.props.type} orc-${this.props.status}`} style={{
             top: this.props.position.top + 'px',
             right: this.props.position.right + 'px'
         }}
-        ></div>
+        >
+            <div className="enemy-status" style={{
+                width: this.props.position.width + 'px',
+                padding: "0 " + this.props.approximation.X + "px"
+            }}>
+                <div className="enemy-name">{this.props.name}</div>
+                <div className="hp-bar">
+                    <div className="hp-value" style={{
+                        width: (this.props.hp / this.props.value) * 100 + '%',
+                    }}></div>
+                </div>
+            </div>
+
+        </div>
     );
   }
 }
